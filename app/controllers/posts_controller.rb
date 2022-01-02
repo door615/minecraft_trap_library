cf95191255daa8928245f1ea770a30b2e95c891a
sudo takeo <xutengjiansheng@gmail.com>
2022-01-02 17:25:00 +0900

    Postモデルを追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,29 @@
 class PostsController < ApplicationController
-  def new
+  # 記事の一覧表示
+  def index
   end
 
+  # 記事の表示
   def show
   end
 
+  # 記事の作成
+  def new 
+  end
+
+  # 記事の登録
+  def create
+  end
+
+  # 記事の編集
   def edit
   end
 
+  # 記事の更新
+  def update
+  end
+
+  # 記事の削除
   def destroy
   end
 end

7c4e698e45b64ff079cad9b45dfc6c24b809b265
sudo takeo <xutengjiansheng@gmail.com>
2022-01-02 19:26:46 +0900

    index,show,new,createを作った

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,34 @@
 class PostsController < ApplicationController
   # 記事の一覧表示
   def index
+    @posts = Post.all
   end
 
   # 記事の表示
   def show
+    @post = Post.find(params[:id])
   end
 
   # 記事の作成
   def new 
+    @post = Post.new
   end
 
   # 記事の登録
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+
+    private
+
+    def post_params
+      params.require(:post).permit(:title, :link, :feature_text,
+                                   :text)
+    end
   end
 
   # 記事の編集

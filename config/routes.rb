a6d033200292e0a5dc7383279215e3189afb8c5f
sudo takeo <xutengjiansheng@gmail.com>
2022-01-02 16:51:56 +0900

    Postsコントローラーを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'posts/new'
+  get 'posts/show'
+  get 'posts/edit'
+  get 'posts/destroy'
   root 'web_pages#home'
   get '/about', to: 'web_pages#about' 
 end

56bd7e7063c2ee23dd9cc3ab3aa5d44d6b346380
sudo takeo <xutengjiansheng@gmail.com>
2022-01-12 16:08:30 +0900

    タグの名前を取り出す試しのコードを書いた

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,7 +24,7 @@ class PostsController < ApplicationController
   def create
       
     @post = Post.new(post_params)
-    if @post.save && @tag.save
+    if @post.save
       redirect_to @post
     else
       render 'new'

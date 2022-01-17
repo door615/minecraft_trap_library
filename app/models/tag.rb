5f7c56c3a69e6b1adcc1ed7cb9a754657f6c14cb
sudo takeo <xutengjiansheng@gmail.com>
2022-01-17 17:44:06 +0900

    注釈を追加

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,8 +2,14 @@ class Tag < ApplicationRecord
     has_many :post_tags, dependent: :destroy
     has_many :posts, through: :post_tags
 
+
+    #検索フォームでタグが入力されたとき、タグをOR検索します
+    #このメソッドは同じデータを複数回データベースから取り出しているので
+    #改善が必要です
     def self.search(tag_ids)
 
+            #フォームから来たタグのパラメーターが["", "1"]のように最初に空白があった
+            #ので、最初の値を削除しています。(ストロングパラメーターにtag_ids: []と書いたせい？)
             tag_ids.delete_at(0)
             array2 = []
             tag_ids.each do |tag_id|

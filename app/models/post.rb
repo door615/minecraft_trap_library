894d91a48565887dfd93a29d0fbcf17d7199a461
sudo takeo <xutengjiansheng@gmail.com>
2022-01-17 17:47:19 +0900

    注釈を追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,8 +5,13 @@ class Post < ApplicationRecord
     validates :feature_text, presence: true
     mount_uploader :avatar, AvatarUploader
 
+    #検索フォームから送られてきた検索ワードが、タイトルもしくは本文に含まれていたら
+    #値を返します
     def self.search(keyword)
         array1 = Post.where(["title like? OR feature_text like?", "%#{keyword}%", "%#{keyword}%"])
+        #タグの検索結果とAND検索をしたいので配列に変換しています
+        #これだとデータ数が増えたときに一気にすべてのデータを取り出しているので
+        #処理速度が遅くなるので改善が必要です
         array1.to_a
     end
 end

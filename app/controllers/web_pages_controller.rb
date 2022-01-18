ac44ab387ce2b1f4b213e495be08467d51d27b94
sudo takeo <xutengjiansheng@gmail.com>
2022-01-18 20:04:10 +0900

    検索をwebpagesコントローラーに移した

--- app/controllers/web_pages_controller.rb
+++ app/controllers/web_pages_controller.rb
@@ -4,4 +4,41 @@ class WebPagesController < ApplicationController
 
   def about
   end
+
+  #検索フォームを書いているページです
+  def search_page
+  end
+
+  def search
+    
+    keyword = params[:keyword]
+    tag_ids = params[:tag_ids]
+    
+    #ifの条件式の部分はフォームに何も入力しなかったとき、keyword = "", tag_ids = [""]となっていたので
+    #このような書き方になってしまいました...
+    #もっとましな書き方にできるようにしたいです
+
+    #検索フォームにキーワードとタグが両方とも入力されていたら、キーワードのあいまい検索
+    #とタグ検索をAND検索します。取り出すデータの重複、一度にすべてのデータを取り出している
+    #などの改善点があります。
+    if keyword != "" && tag_ids != [""]
+      array1 = Post.search(keyword)
+      array2 = Tag.search(tag_ids)
+      posts = array1 & array2
+      posts.uniq!
+      @posts = Kaminari.paginate_array(posts).page(params[:page]).per(10)
+
+    elsif keyword != ""  
+      @posts = Kaminari.paginate_array(Post.search(keyword)).page(params[:page]).per(10)
+
+    elsif tag_ids != [""]
+      @posts = Kaminari.paginate_array(Tag.search(tag_ids)).page(params[:page]).per(10)
+
+    else 
+      @posts = Post.page(params[:page]).per(10)
+      
+    end
+    render 'posts/index'
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :posts
   root 'posts#index'
   get '/about', to: 'web_pages#about' 
-  get '/search_page', to: "posts#search_page"
-  get 'search', to: 'posts#search'
+  get '/search_page', to: "web_pages#search_page"
+  get 'search', to: 'web_pages#search'
   get "auth", to: "auths#auth"
   post 'login', to: 'auths#login'
 end

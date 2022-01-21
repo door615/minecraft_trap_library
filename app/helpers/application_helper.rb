39a138605b278987ce4723acb6e4d83a8d61d388
sudo takeo <xutengjiansheng@gmail.com>
2022-01-22 01:20:02 +0900

    タイトルとディスクリプションを作った(まだ使用不可、使うにはタグの検索メソッドの返り値を配列にしないようにしなければならない)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,36 @@
 module ApplicationHelper
+
+    #ページのタイトルを作るメソッドです。
+    def full_title(page_title = '')
+        base_title = "Minecraftトラップライブラリー"
+        if page_title.empty?
+          "#{base_title} | マイクラのトラップをまとめました"
+        else
+          "#{page_title} | #{base_title}"
+        end
+    end
+
+      #ページのディスクリプションを作るメソッドです。
+    def full_description(page_description = '')
+      base_description = "数多あるマイクラのトラップをまとめました！
+      java版用、統合版用、簡単に作れるものから最高効率のものまであらゆるトラップが
+      揃っています。豊富なタグで分類しているのであなたの求めるトラップが見つかります！"
+
+      if page_description.empty?
+
+        base_description
+
+      #provideされた文字列にトラップが含まれている場合、以下の文章になります
+      elsif paga_description.match(/.*?トラップ.*?/)
+
+          "#{page_description}の一覧ページです。あなたが作りたい
+          #{page_description}はどれですか？"
+
+      else
+          
+        "#{page_description}のトラップ一覧ページです。あなたが作りたい
+        #{page_description}のトラップはどれですか？"
+
+      end
+    end
 end

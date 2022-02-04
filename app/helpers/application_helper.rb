59365455d46667bf28464a19eb514edfe4205903
sudo takeo <xutengjiansheng@gmail.com>
2022-02-04 22:57:34 +0900

    タイトルとディスクリプションを作った

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,26 +10,33 @@ module ApplicationHelper
         end
     end
 
-      #ページのディスクリプションを作るメソッドです。
+      #トップページとサイドバーからアクセスできるページのディスクリプションを作るメソッドです。
     def full_description(page_description = '')
       base_description = "数多あるマイクラのトラップをまとめました！
       java版用、統合版用、簡単に作れるものから最高効率のものまであらゆるトラップが
       揃っています。豊富なタグで分類しているのであなたの求めるトラップが見つかります！"
 
+      #トップページのディスクリプションはこれになります。
       if page_description.empty?
 
         base_description
 
       #provideされた文字列にトラップが含まれている場合、以下の文章になります
-      elsif paga_description.match(/.*?トラップ.*?/)
+      elsif page_description.match(/.*?トラップ.*?/)
 
           "#{page_description}の一覧ページです。あなたが作りたい
           #{page_description}はどれですか？"
 
-      else
+      #タグで探す　java,統合版のディスクリプションです。
+      elsif page_description.match(/.*?版.*?/)
           
         "#{page_description}のトラップ一覧ページです。あなたが作りたい
         #{page_description}のトラップはどれですか？"
+
+      #サイドバーのabout、contactなどの静的ページのディスクリプションです。
+      else
+
+        "#{page_description}"
 
       end
     end
